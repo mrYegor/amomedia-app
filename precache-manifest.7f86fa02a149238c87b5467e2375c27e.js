c2432ada06196351a2fb359cddf0b20438c13fe3
Yehor Markevych <mr.yegor@yahoo.com>
2020-01-21 16:44:31 +0200

    Updates

---
+++ precache-manifest.7f86fa02a149238c87b5467e2375c27e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61dc55082f7821ee9cde578fbd7c1120",
+    "url": "/amomedia-app/index.html"
+  },
+  {
+    "revision": "64b51ad6e20592209ff6",
+    "url": "/amomedia-app/static/css/main.43fb368b.chunk.css"
+  },
+  {
+    "revision": "585cf5f7cb7d2594068f",
+    "url": "/amomedia-app/static/js/2.bc8a31d4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/amomedia-app/static/js/2.bc8a31d4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "64b51ad6e20592209ff6",
+    "url": "/amomedia-app/static/js/main.f0f8f3ac.chunk.js"
+  },
+  {
+    "revision": "9eb026e80de40f2db0fa",
+    "url": "/amomedia-app/static/js/runtime-main.34260742.js"
+  },
+  {
+    "revision": "d6166bd0d4728d5e7b7bc0aa2c8bca8d",
+    "url": "/amomedia-app/static/media/dish.d6166bd0.png"
+  },
+  {
+    "revision": "34d28a179a1deb58562870564c624996",
+    "url": "/amomedia-app/static/media/dish1.34d28a17.png"
+  }
+]);
